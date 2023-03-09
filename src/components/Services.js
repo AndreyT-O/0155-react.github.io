0e4c8f617adb2e35f8b0f3c9911ba45962e7479d
AndreyT <trufikao@gmail.com>
2023-03-10 00:40:13 +0200

    changed table to list

--- src/components/Services.js
+++ src/components/Services.js
@@ -2,41 +2,29 @@ import '../scss/components/services.scss';
 
 function Services(props) {
 
-    const listItems = props.services.items.map((el, index) =>
-        <tr className="services__row" key={index}>
-            <td className="services__name" style={{'--interest': el.interest + '%'}}>
-                <span>{ el.name }</span>
-            </td>
-            <td className="services__result">
-                <span>{ el.amount }</span>
-            </td>
-        </tr>
-    );
-
     return (
-        <table className={ 'services ' + props.className ?? '' }>
-            <thead className="services__head">
-                <tr className="services__row">
-                    <th></th>
-                    <th className="services__title">
-                        <span>Services</span>
-                    </th>
-                </tr>
-            </thead>
-            <tbody className="services__body">
-                { listItems }
-            </tbody>
-            <tfoot className="services__foot">
-                <tr className="services__row">
-                    <td className="services__total">
-                        <span className="title">Total</span>
-                    </td>
-                    <td className="services__amount">
-                        <span className="title">{ props.services.total }</span>
-                    </td>
-                </tr>
-            </tfoot>
-        </table>
+        <ul className={ 'services ' + props.className ?? ''}>
+            <li className='services__head'>
+                <span className='services__title'>Services</span>
+            </li>
+            { props.services.items.map((el, index) => 
+                <li 
+                    className={'services__info' + (index === props.services.items.length - 1 ? ' services__info--last' : '')} 
+                    key={index} 
+                    style={{'--interest': el.interest + '%'}}>
+                    <span className='services__name'>{ el.name }</span>
+                    <span className='services__result'>{ el.amount }</span>
+                </li>
+            ) }
+            <li className='services__foot'>
+                <span className='services__total'>
+                    Total
+                </span>
+                <span className='services__amount'>
+                    { props.services.total }
+                </span>
+            </li>
+        </ul>
     );
 }
 
